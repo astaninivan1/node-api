236925a23986e2013921472a5e13547977ca44ac
Астанин Иван <iastanin@softrust.ru>
2021-04-01 19:59:58 +0300

    mongodb install

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
+const mongoClient = new MongoClient('mongodb://localhost:27017/', {useUnifiedTopology: true});
+mongoClient.connect((err, client) => {
+   const db = client.db("usersdb");
+   const collection = db.collection("users");
+   let user = {name: "Tom", age: 23};
+   collection.insertOne(user, function(err, result){
+
+      if(err){
+         return console.log(err);
+      }
+      console.log(result.ops);
+      client.close();
+   });
+});
 
 app.get('/', (req, res) => {
    res.send('<h1>Hello</h1>')
